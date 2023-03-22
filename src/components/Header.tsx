53eec1437665965bf5747ad9dd81092a44600fc1
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2023-03-22 20:08:47 -0300

    feat: initial commit and header

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <strong>Social Media</strong>
+
+    </header>
+  )
+}
