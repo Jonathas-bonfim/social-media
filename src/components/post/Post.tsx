baaaf9bede8b46a57129a99dc07b9d142f0ca160
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2023-03-23 08:51:59 -0300

    feat: post structure

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,3 +1,5 @@
+import styles from './Post.module.scss'
+
 type PostProps = {
   author: string,
   content: string
@@ -5,9 +7,34 @@ type PostProps = {
 
 export function Post({ author, content }: PostProps) {
   return (
-    <>
-      <h3>{author}</h3>
-      <p>{content}</p>
-    </>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/jonathas-bonfim.png" alt="Avatar"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Jônathas Bonfim</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="23 de Março às 8:15h" dateTime='2023-03-23 08:00:00'>Publicado a 30 minutos</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Olá pessoal, espero que estejam bem</p>
+        <p>Desenvolvi a aplicação social media, é um feed de rede social com vários posts, irei deixar o link do repositório com mais informações</p>
+        <p><a href="#">https://github.com/Jonathas-bonfim/social-media</a></p>
+        <p>
+          <a href="">#dev #frontend #github</a>
+        </p>
+      </div>
+
+
+
+
+
+    </article>
   )
 }
